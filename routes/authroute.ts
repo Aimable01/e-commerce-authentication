fdbc375ac59da569c35e3257292f220012c0c949
unknown <aimable.kwizera14@gmail.com>
2024-03-29 08:57:48 +0200

    the register login route

--- routes/authroute.ts
+++ routes/authroute.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { register } from "../controllers/authControllers";
+import { login } from "../controllers/authControllers";
 
 export const authRouter = express.Router();
 authRouter.post("/register", register);
+authRouter.post("/login", login);
